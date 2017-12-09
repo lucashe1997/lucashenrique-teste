921c4c7d758c176f03f62e5bfc9939dade5fdc50
Lucas Henrique <lucashe1997@gmail.com>
2017-12-09 21:41:39 +0000

    Master Commit

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $servidor = 'localhost';
 $usuario = 'empresa';
 $senha = 'empresa';
-$banco = 'empresa';
+$banco = 'SENHA';
 
 setlocale(LC_ALL, "pt_BR", "pt_BR.iso-8859-1", "pt_BR.utf-8", "portuguese");
 date_default_timezone_set('America/Sao_Paulo');
